104d3b20a1e8662865d5f58ba37e3c05f0fb2e69
Mehdi Zerouak <118940145+Mehdiz04@users.noreply.github.com>
2023-04-19 02:24:59 +0100

    Update main.py

--- main.py
+++ main.py
@@ -10,18 +10,18 @@ set3 = {0 , 1 , 2}
 with open(r'C:\Users\zerou\Documents\PythonWS\MC_challenge_3\doc.txt' , 'r') as document:
     doc = document.readlines()
     for line in doc:
-        i , j = 1 , 1
+        r , c = 1 , 1
         for i in range(len(line)):
-            if (line[i] == 'U') and (i+1 in set3):
-                i += 1
-            elif (line[i] == 'D') and (i-1 in set3):
-                i -= 1
-            elif (line[i] == 'R') and (j+1 in set3):
-                j += 1
-            elif (line[i] == 'L') and (j-1 in set3):
-                j -= 1
+            if (line[i] == 'U') and (r+1 in set3):
+                r += 1
+            elif (line[i] == 'D') and (r-1 in set3):
+                r -= 1
+            elif (line[i] == 'R') and (c+1 in set3):
+                c += 1
+            elif (line[i] == 'L') and (c-1 in set3):
+                c -= 1
             else:
-                code += keypad[i][j]
+                code += keypad[r][c]
                 break
     
     print(code)
