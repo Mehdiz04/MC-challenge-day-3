9190e92c3dfae732ded9a08ddfdc025d1a4f2711
Mehdi Zerouak <118940145+Mehdiz04@users.noreply.github.com>
2023-04-21 00:36:02 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+#fixed
 keypad = [  [ '1' , '2' , '3'] ,
             [ '4' , '5' , '6'] ,
             [ '7' , '8' , '9'] ]
@@ -7,21 +8,19 @@ code = ''
 #cuz searching for an element in a set is O(1)
 set3 = {0 , 1 , 2}
 
+movements = {'U':[1 , 0] , 'D':[-1 , 0] , 'R':[0 , 1] , 'L':[0 , -1]}
+
 with open(r'C:\Users\zerou\Documents\PythonWS\MC_challenge_3\doc.txt' , 'r') as document:
     doc = document.readlines()
     for line in doc:
         r , c = 1 , 1
-        for i in range(len(line)):
-            if (line[i] == 'U') and (r+1 in set3):
-                r += 1
-            elif (line[i] == 'D') and (r-1 in set3):
-                r -= 1
-            elif (line[i] == 'R') and (c+1 in set3):
-                c += 1
-            elif (line[i] == 'L') and (c-1 in set3):
-                c -= 1
+        for l in line[:-1]:
+            if (r + movements[l][0] in set3) and (c + movements[l][1] in set3):
+                r += movements[l][0]
+                c += movements[l][1]
             else:
-                code += keypad[r][c]
-                break
-    
+                continue
+
+        code += keypad[r][c]
+
     print(code)
